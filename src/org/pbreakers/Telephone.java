22869c68fcf641fa76dd56386d8eb525a1cf1c7d
Dark Angel <jonathanyombo@gmail.com>
2019-02-05 20:42:17 +0200

    Telephone class

---
+++ src/org/pbreakers/Telephone.java
@@ -0,0 +1,33 @@
+package org.pbreakers;
+
+public class Telephone extends Coordonnee {
+    private String numero;
+
+    Telephone(String num){
+        try{
+            this.setNumero(num);
+        }catch(ValeurErroneeException e){
+            System.out.println("Numero de telephone Incorrecte : ");
+        }
+
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) throws ValeurErroneeException {
+        if (numero.equals("")){
+            throw new ValeurErroneeException("");
+        }else if (numero.length() < 10){
+            throw new ValeurErroneeException();
+        }else{
+            this.numero = numero;
+        }
+    }
+
+    @Override
+    public void utiliser() {
+
+    }
+}
