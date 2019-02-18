f405aa5152efcf4165db8298011ec1d26c378f0e
Dark Angel <jonathanyombo@gmail.com>
2019-02-18 12:50:56 +0200

    change attributes call

--- src/org/pbreakers/Main.java
+++ src/org/pbreakers/Main.java
@@ -11,23 +11,28 @@ public class Main {
             p4 = new Personne("Jonathan");
             p5 = new Personne("Josiane");
 
-            p1.setAdresse(new Adresse("3011","kinkondja","gambela","lubumbashi","lubumbashi"));
-            p2.setAdresse(new Adresse("3011","kinkondja","gambela","lubumbashi","lubumbashi"));
-            p3.setAdresse(new Adresse("3011","kinkondja","gambela","lubumbashi","lubumbashi"));
-            p4.setAdresse(new Adresse("3011","kinkondja","gambela","lubumbashi","lubumbashi"));
-            p5.setAdresse(new Adresse("3011","kinkondja","gambela","lubumbashi","lubumbashi"));
+            p1.adresses.add(new Adresse("3011","kinkondja","gambela","lubumbashi","lubumbashi"));
+            p2.adresses.add(new Adresse("3011","kinkondja","gambela","lubumbashi","lubumbashi"));
+            p3.adresses.add(new Adresse("3011","kinkondja","gambela","lubumbashi","lubumbashi"));
+            p4.adresses.add(new Adresse("3011","kinkondja","gambela","lubumbashi","lubumbashi"));
+            p5.adresses.add(new Adresse("3011","kinkondja","gambela","lubumbashi","lubumbashi"));
 
-            p1.setPhones(new Telephone("0991888702"), new Telephone("+243823020372"));
-            p2.setPhones(new Telephone("0992145447"), new Telephone("+243999931399"));
-            p3.setPhones(new Telephone("0991856321"), new Telephone("+243909009000"));
-            p4.setPhones(new Telephone("0994445588"), new Telephone("+243823004007"));
-            p5.setPhones(new Telephone("0991880902"), new Telephone("+243810550590"));
+            p1.phones.add(new Telephone("0991888702"));
+            p1.phones.add( new Telephone("+243823020372"));
+            p2.phones.add(new Telephone("0992145447") );
+            p2.phones.add(new Telephone("+243999931399"));
+            p3.phones.add(new Telephone("0991856321") );
+            p3.phones.add(new Telephone("+243909009000"));
+            p4.phones.add(new Telephone("0994445588") );
+            p4.phones.add(new Telephone("+243823004007"));
+            p5.phones.add(new Telephone("0991880902"));
+            p5.phones.add( new Telephone("+243810550590"));
 
-            p1.setEmail(new Email("jonathanyombo@gmail.com"));
-            p2.setEmail(new Email("jonathanyombo@yahoo.com"));
-            p3.setEmail(new Email("jonathanyombo@facebook.com"));
-            p4.setEmail(new Email("jonathanyombo@outlook.com"));
-            p5.setEmail(new Email("jonathanyombo@pbreakers.com"));
+            p1.email.add(new Email("jonathanyombo@gmail.com"));
+            p2.email.add(new Email("jonathanyombo@yahoo.com"));
+            p3.email.add(new Email("jonathanyombo@facebook.com"));
+            p4.email.add(new Email("jonathanyombo@outlook.com"));
+            p5.email.add(new Email("jonathanyombo@pbreakers.com"));
 
             Carnet c1 = new Carnet(p1);
             Carnet c2 = new Carnet(p2);
